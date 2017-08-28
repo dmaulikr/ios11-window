26e343aa6a53bf3fd8c5ad2bbed38627f9f0dda1
Thomas Pickup <tpickup01@qub.ac.uk>
2017-08-28 15:31:43 +0100

    Added in textboxes and calculate button

--- Window Shopper/Window Shopper/Controller/vcMain.swift
+++ Window Shopper/Window Shopper/Controller/vcMain.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class vcMain: UIViewController {
-
+    @IBOutlet weak var txtWage: tfCurrency!
+    @IBOutlet weak var txtPrice: tfCurrency!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let btnCalc = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        btnCalc.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        btnCalc.setTitle("Calculate", for: .normal)
+        btnCalc.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        btnCalc.addTarget(self, action: #selector(vcMain.caclulate), for: .touchUpInside)
+        
+        txtWage.inputAccessoryView = btnCalc
+        txtPrice.inputAccessoryView = btnCalc
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func caclulate() {
+        print("we got here")
     }
-
-
 }
 
