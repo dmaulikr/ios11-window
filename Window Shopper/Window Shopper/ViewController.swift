4ef6be73f3c9c11d6979f5c1072a11aadf2339ed
Thomas Pickup <tpickup01@qub.ac.uk>
2017-08-28 13:16:30 +0100

    Initial Commit - Project Created

---
+++ Window Shopper/Window Shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Window Shopper
+//
+//  Created by Thomas Pickup on 28/08/2017.
+//  Copyright © 2017 Thomas Pickup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
